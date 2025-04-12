369887c6757769a35f68cc9a9c4ebdd45cb7fd19
FREDDYSTICAS <cfreddystiven@gmail.com>
2025-04-11 19:18:17 -0500

    Corregido la importación de clientes.controller

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const clienteController = require('../controllers/clientes.controller');
+const clienteController = require('../controllers/clientes.Controller');
 
 // API CRUD
 router.get('/', clienteController.listarClientes);
